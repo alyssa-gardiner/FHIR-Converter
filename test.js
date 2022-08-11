f4067943cb7ffce27b5283e1910e5e4fb597f15a
Alyssa Gardiner <agardiner@onemedical.com>
2022-08-11 11:16:02 -0400

    testing

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
-function test() {
-    return "This is the actual thing that I want to return like do you have access to it";
-}
-// test();
+const WorkerPool = require("./src/lib/workers/workerPool");
+const fs = require('fs');
 
-process.stdout.write(test());
-// console.log(test());
+async function test() {
+    let magic;
+    if(process.env.npm_config_myVar === '' || process.env.npm_config_myVar === null || process.env.npm_config_myVar === undefined) {
+        return "Are you dumb?";
+    } else {
+        const xmlFile = fs.readFileSync(process.env.npm_config_myVar, 'utf8');
+        const workerPool = new WorkerPool('./src/lib/workers/worker.js', require('os').cpus().length);
+        return workerPool.exec({
+            'type': '/api/convert/:srcDataType/:template',
+            'srcData': xmlFile.toString(),
+            'srcDataType': "cda",
+            'templateName': "ccd.hbs"
+        }).then((result) => {
+            const resultMessage = result.resultMsg;
+            magic = JSON.stringify(resultMessage);
+            console.log(magic);
+            return magic;
+        }).then(() => {
+            workerPool.destroy();
+        });
+    }
+}
+
+console.log(test());
