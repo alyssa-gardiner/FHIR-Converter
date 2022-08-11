904166db0aaff8862f07a084f46f09a525346def
Alyssa Gardiner <agardiner@onemedical.com>
2022-08-11 10:31:03 -0400

    Testing

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 function test() {
     return "This is the actual thing that I want to return like do you have access to it";
 }
-test();
+// test();
+
+process.stdout.write(test());
 // console.log(test());
