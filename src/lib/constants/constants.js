4511c0f1a67723c099b89feba57d8c39e8a3c858
Alyssa Gardiner <agardiner@onemedical.com>
2022-08-15 13:02:59 -0400

    WIP

--- src/lib/constants/constants.js
+++ src/lib/constants/constants.js
@@ -9,7 +9,7 @@ let serviceTemplateFolder = '../../service-templates';
 let sampleDataFolder = '../../sample-data';
 
 module.exports.BASE_TEMPLATE_FILES_LOCATION = path.join(__dirname, '../../templates');
-module.exports.TEMPLATE_FILES_LOCATION = path.join(__dirname, serviceTemplateFolder);
+module.exports.TEMPLATE_FILES_LOCATION = path.join(__dirname, '../../templates');
 module.exports.SAMPLE_DATA_LOCATION =  path.join(__dirname, sampleDataFolder);
 module.exports.STATIC_LOCATION = path.join(__dirname, '../../static');
 module.exports.CODE_MIRROR_LOCATION = path.join(__dirname, '../../../node_modules/codemirror/');
