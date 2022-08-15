1a660924b64c30606c7bcb95fe28b7a8ef70f0f8
Alyssa Gardiner <agardiner@onemedical.com>
2022-08-15 12:34:41 -0400

    Ttest

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ const WorkerPool = require("./lib/workers/workerPool");
 
 const createAndUploadFhirResource = () => {
     if(process.env.npm_config_myVar === '' || process.env.npm_config_myVar === null || process.env.npm_config_myVar === undefined) {
-        return "Are you dumb?";
+        return "You need a file path";
     } else {
         const xmlFile = fs.readFileSync(process.env.npm_config_myVar, 'utf8');
         const workerPool = new WorkerPool('./src/lib/workers/worker.js', require('os').cpus().length);
